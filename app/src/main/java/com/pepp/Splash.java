c72dde11c1ff41356be9e9fd7207624617bea83a
mahendra dabi <mahendradabi26@gmail.com>
2018-02-04 23:29:25 +0530

    Initial commit

---
+++ app/src/main/java/com/pepp/Splash.java
@@ -0,0 +1,27 @@
+package com.pepp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by MAHI on 04-Feb-18.
+ */
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splash.this, MainActivity.class));
+            }
+        }, 1000);
+
+    }
+}
